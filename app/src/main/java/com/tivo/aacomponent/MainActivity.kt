25822d3b3c93df3e7eef26056d93173c84d32fa2
Krzysztof Betlej <krzysztof.betlej@tivo.com>
2019-09-19 16:30:15 +0200

    data binding

--- app/src/main/java/com/tivo/aacomponent/MainActivity.kt
+++ app/src/main/java/com/tivo/aacomponent/MainActivity.kt
@@ -4,9 +4,10 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.tivo.aacomponent.adapter.PlantListAdapter
+import com.tivo.aacomponent.databinding.ActivityMainBinding
 import com.tivo.aacomponent.repository.PlantRepository
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -18,30 +19,32 @@ class MainActivity : Activity() {
     //Inject repository
     val repository: PlantRepository by inject()
     val disposable = CompositeDisposable()
-    private val adapter: PlantListAdapter = PlantListAdapter(R.layout.plant_list_view_item, listOf(null))
+    lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: PlantListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // change this to use view model
-        setContentView(R.layout.activity_main)
+        // change this to use data binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         initPlantList()
     }
 
     private fun initPlantList() {
-        disposable.addAll(repository.getPlants()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { plants, s ->
-                adapter.setPlants(plants)
-            })
+        adapter = PlantListAdapter(R.layout.plant_list_view_item, listOf(null))
+        binding.plantsList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.plantsList.adapter = adapter
         adapter.clickListener = { view ->
             val plantId = view.findViewById<View>(R.id.plant_id).tag as Int
             val intent = Intent(this, PlantDetailsActivity::class.java)
             intent.putExtra(PlantDetailsActivity.PLANT_ID_TAG, plantId)
             startActivity(intent)
         }
-        val recycler = findViewById<RecyclerView>(R.id.plants_list)
-        recycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        recycler.adapter = adapter
+
+        disposable.addAll(repository.getPlants()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { plants, s ->
+                adapter.setPlants(plants)
+            })
     }
 }
