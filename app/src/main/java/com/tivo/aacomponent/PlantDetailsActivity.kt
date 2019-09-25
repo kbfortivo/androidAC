124011722ad7e29c91f2d5cf66f84c1b864997f2
Krzysztof Betlej <krzysztof.betlej@tivo.com>
2019-09-25 09:09:42 +0200

    binding adapter for multi attribute

--- app/src/main/java/com/tivo/aacomponent/PlantDetailsActivity.kt
+++ app/src/main/java/com/tivo/aacomponent/PlantDetailsActivity.kt
@@ -2,12 +2,9 @@ package com.tivo.aacomponent
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
-import com.tivo.aacomponent.databinding.ActivityMainBinding
 import com.tivo.aacomponent.databinding.PlantDetailsViewBinding
 import com.tivo.aacomponent.repository.PlantRepository
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -40,27 +37,7 @@ class PlantDetailsActivity : Activity() {
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe {
                 plant, s ->
-                plant?.let {
-                    binding.plant = it
-
-                    val plantScientific = findViewById<TextView>(R.id.plant_scientific_name)
-                    plantScientific.text = getString(R.string.plant_scientific_template_text, it.scientific_name)
-                    val plantType = findViewById<TextView>(R.id.plant_type)
-                    plantType.text = getString(R.string.plant_type_template_text, it.genus?.name ?: "")
-                    val plantFamily = findViewById<TextView>(R.id.plant_family)
-                    plantFamily.text = getString(R.string.plant_family_template_text, it.family_common_name ?: "")
-                    val plantSpecies = findViewById<TextView>(R.id.plant_species)
-                    plantSpecies.text = getString(R.string.plant_species_template_text, it.species.name ?: it.species.scientific_name)
-                    if (plant.images.isNotEmpty()) {
-                        Glide
-                            .with(applicationContext)
-                            .load(plant.images[0].url)
-                            .placeholder(R.drawable.rose_img)
-                            .into(binding.plantImage)
-                    }
-                }
-
-
+                binding.plant = plant
             })
     }
 
